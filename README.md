007cd67fe0f8aa191f4a48c70cc0bb929cde2363
Adrian Del Maestro <adrian@delmaestro.org>
2022-01-23 14:13:32 -0500

    initial input of ml4s project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ml4s
 Helper functions for introductory machine learning for sciences course.
+
+## Installation
+    pip install git+git://github.com/DelMaestroGroup/ml4s.git#egg=ml4s
+
+## Requirements
+    numpy, matplotlib, viznet 
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='ml4s',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    description='A python package implenting useful utilities for an introductory machine learning course taught at the University of Tennessee.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['numpy','matplotlib','viznet'],
+    python_requires='>=3.6',
+    url='https://github.com/DelMaestroGroup/ml4s',
+    author='Adrian Del Maestro',
+    author_email='adrian@delmaestro.org',
+    classifiers=[
+   'License :: OSI Approved :: MIT License',
+   'Programming Language :: Python :: 3.6',
+   'Programming Language :: Python :: 3.7',
+   'Topic :: Scientific/Engineering :: Physics']
+)
